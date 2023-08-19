554b69cb86c0f39fd8d3dc709736eebef92073eb
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2023-08-19 13:58:05 +0900

    feat: try to use touches

--- src/inputs/index.ts
+++ src/inputs/index.ts
@@ -3,9 +3,13 @@ import p5 from 'p5';
 /* inputs
  * - keyboard
  * - mouse
+ * - touch
  * - pen
  * - camera
  * - mic
+ * outputs
+ * - render
+ * - images
  */
 
 document.body.style.margin = "0";
@@ -27,13 +31,17 @@ export const sketch = (p: p5) => {
     }
     p.text(p.key, 33, 65);
 
-    // mouse
+    // mouse, and, pen
     if (p.mouseIsPressed) {
       p.fill(0);
     } else {
       p.fill(255);
     }
     p.ellipse(p.mouseX, p.mouseY, 80, 80);
+
+    if (p.touches.length) {
+      console.log(p.touches);
+    }
   }
 };
 
