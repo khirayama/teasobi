c2ea333ca6d777eda7b57c0fafb2ef90a86d91a4
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2023-08-20 09:32:11 +0900

    feat: display images

--- src/inputs/index.ts
+++ src/inputs/index.ts
@@ -15,13 +15,27 @@ import p5 from 'p5';
 document.body.style.margin = "0";
 
 export const sketch = (p: p5) => {
+  let img = null;
+  let imageCORS = null;
+
+  p.preload = () => {
+    imageCORS = new Image();
+    imageCORS.src = 'https://placehold.jp/150x150.png';
+    imageCORS.width = 150;
+    imageCORS.height = 150;
+
+    img = p.loadImage('./150x150.png');
+  }
+
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.textSize(50);
+     p.drawingContext.drawImage(imageCORS, 10, 10, 150, 150);
   }
 
   p.draw = () => {
     // p.background(200);
+    p.image(img, 160, 10, 150, 150);
 
     // keyboard
     if (p.keyIsPressed === true) {
