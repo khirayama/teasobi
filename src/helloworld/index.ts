fc61d35b91e64a3ed498b187179d62f88461e43f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2023-09-13 07:18:28 +0900

    style: apply prettier

--- src/helloworld/index.ts
+++ src/helloworld/index.ts
@@ -1,12 +1,12 @@
 // [get started | p5.js](https://p5js.org/get-started/)
-import p5 from 'p5';
+import p5 from "p5";
 
 document.body.style.margin = "0";
 
 export const sketch = (p: p5) => {
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-  }
+  };
   p.draw = () => {
     if (p.mouseIsPressed) {
       p.fill(0);
@@ -14,7 +14,7 @@ export const sketch = (p: p5) => {
       p.fill(255);
     }
     p.ellipse(p.mouseX, p.mouseY, 80, 80);
-  }
+  };
 };
 
 new p5(sketch, document.body);
